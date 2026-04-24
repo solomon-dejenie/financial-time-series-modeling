649b8a52f6d0c39784fd1f3657e0f89cf599d0be
Solomon Dejenie <soldm7@yahoo.com>
2026-04-24 17:26:27 +0300

    Update vecm_model.py

--- models/vecm_model.py
+++ models/vecm_model.py
@@ -1,1 +1,9 @@
 
+## Purpose
+
+### Model equilibrium relationships
+from statsmodels.tsa.vector_ar.vecm import VECM
+
+def run_vecm(data):
+    model = VECM(data, k_ar_diff=1, coint_rank=1)
+    return model.fit()
