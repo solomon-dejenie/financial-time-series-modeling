064ccfbd364abfebc696803b00ab5d8a800abd82
Solomon Dejenie <soldm7@yahoo.com>
2026-04-24 17:52:53 +0300

    Refactor run_pca function for clarity and consistency

--- models/pca_model.py
+++ models/pca_model.py
@@ -2,7 +2,8 @@
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-def run_pca(X, n=2):
-    Xs = StandardScaler().fit_transform(X)
-    pca = PCA(n_components=n)
-    return pca.fit_transform(Xs), pca
+def run_pca(X,n_components=2):
+    Xs=StandardScaler().fit_transform(X)
+    pca=PCA(n_components=n_components)
+    comps=pca.fit_transform(Xs)
+    return comps,pca
