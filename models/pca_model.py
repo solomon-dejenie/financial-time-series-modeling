fac5143534bfcfd4e6ea9cabdf23b0d06643724d
Solomon Dejenie <soldm7@yahoo.com>
2026-04-24 17:27:05 +0300

    Add PCA implementation with data scaling

--- models/pca_model.py
+++ models/pca_model.py
@@ -1,1 +1,8 @@
 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+def run_pca(X, n=2):
+    Xs = StandardScaler().fit_transform(X)
+    pca = PCA(n_components=n)
+    return pca.fit_transform(Xs), pca
